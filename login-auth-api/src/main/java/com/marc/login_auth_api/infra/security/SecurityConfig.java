fc83003505c678a7d6896cf3550282c2230631b5
Marc-brm <bremen.mcn@gmail.com>
2025-03-12 15:55:20 -0300

    :sparkles: feat: add cors config for frontend communication

--- login-auth-api/src/main/java/com/marc/login_auth_api/infra/security/SecurityConfig.java
+++ login-auth-api/src/main/java/com/marc/login_auth_api/infra/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     private CustomUserDetailsService userDetailsService;
 
     @Autowired
-    SecurityFilter securityFilter;
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,7 +35,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .anyRequest().authenticated())
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors();
         return http.build();
     }
 
