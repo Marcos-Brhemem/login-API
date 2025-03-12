4cbb0b74079b9652f0be2c4c237e8b7b92f1b3fb
Marc-brm <bremen.mcn@gmail.com>
2025-03-12 16:00:54 -0300

    :sparkles: feat: config cors

--- login-auth-api/src/main/java/com/marc/login_auth_api/infra/cors/CorsConfig.java
+++ login-auth-api/src/main/java/com/marc/login_auth_api/infra/cors/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200")); // Permitir frontend
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
 
